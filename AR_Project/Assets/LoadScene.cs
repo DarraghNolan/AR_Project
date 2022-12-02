a32eb1fd873deab2e20d5b4f893dc3a8ab447119
Dazariath <c17366881@mytudublin.ie>
2022-12-02 12:19:09 +0000

    added home scene and button to main scene

---
+++ AR_Project/Assets/LoadScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public int SceneINT;
+
+    public void SwitchScenes()
+    {
+        SceneManager.LoadScene(SceneINT);
+    }
+}
